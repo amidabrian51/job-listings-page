6c1c2ea1d5677343c61d32aa3e31d826f1f217ad
amidabrian51 <briankaty1@me.com>
2020-05-12 12:03:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my React project</h1>
+    </div>
+  );
+}
+
+export default App;
