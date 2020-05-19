003038944410ed5a9f15303eac9cd04247c83f62
amidabrian51 <briankaty1@me.com>
2020-05-19 17:03:39 +0100

    added buttons for languages

--- src/components/job-card/job-card.component.jsx/job-card.component.jsx
+++ src/components/job-card/job-card.component.jsx/job-card.component.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-
+import CustomButton from '../../custom-button/custom.button.component';
 import './job-card.styles.css';
 
-const JobCard = ({company, position, postedAt, contract, location, logo, featured, new: newJob }) => (
-   
+const JobCard = ({company, position, postedAt, contract, location, logo, featured, new: newJob, languages=[], id}) => (
+
 <div className="container">
     <div className='card'>
         <div className='companyName'>
             <img src={logo} alt="logo" width="100" height="100"></img>
         </div>
         <div className='content'>
-        { newJob ? <button className='myButton'>New!</button> : null }
-            
-            {featured ? <button className='myDarkButton'>Featured</button> : null }
+          <div className='newFeatured'>
             <h2>{company}</h2>
+            { newJob ? <button className='myButton'>New!</button> : null }
+            {featured ? <button className='myDarkButton'>Featured</button> : null }
+            </div>
             <h1>{position}</h1>
             <div className='details'>
                 <h3>{postedAt} &#183;</h3>
                 <h3>{contract} &#183;</h3>
                 <h3>{location}</h3>
             </div>
-
+            {languages && languages.map((language, id) =>
+            <CustomButton key={id}> {language}</CustomButton>
+            )}
         </div>
     </div>
 </div>
